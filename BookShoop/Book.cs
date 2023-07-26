29fab19e2dadbf00cf6ea0ca576bd95daef29de3
Furdui Robert <Furdui Robert@DESKTOP-E78TGOH>
2023-07-26 04:45:07 -0700

    Add project files.

---
+++ BookShoop/Book.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShoop
+{
+    internal class Book
+    {
+        public  int Id;
+        public  string name;
+        public string authorName;
+        public string description;
+        public int year;
+        public int price;
+
+        
+        public void ShowBook()
+        {
+            Console.WriteLine(
+                $"{Id}\n"+
+                $"{name}\n" +
+                $"{authorName}\n" +
+                $"{description}\n" +
+                $"{year}\n" +
+                $"{price}");
+        }
+
+        public void AddBook()
+        {
+            Console.WriteLine("Book ID;");
+            Id = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Book Name;");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Book AuthorName;");
+            authorName = Console.ReadLine();
+
+            Console.WriteLine("Book Description;");
+            description = Console.ReadLine();
+
+            Console.WriteLine("Book Year;");
+            year = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Book price;");
+            price = int.Parse(Console.ReadLine());
+        }
+
+    }
+}
---
+++ BookShoop/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using System.Xml.Linq;
+
+namespace BookShoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("BookShoop Management");
+            
+           
+            Book[] book = new Book[1];
+            for(int i  = 0; i < book.Length; i++)
+            {
+                book[i] = Controller();
+            }
+
+            foreach(Book book1 in book)
+            {
+                book1.ShowBook();
+            }
+
+            Console.WriteLine();
+;        }
+        static Book Controller( )
+        {
+            Book result = new Book();
+
+            result.AddBook();
+
+            return result;
+        }
+
+       
+    }
+}
